543980bd262e747e04c5e049f6314bf1dfe7b8b3
Ciscoo91 <franciscomambo91@gmail.com>
2024-11-04 23:57:05 +0100

    Cisco first test

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 const express = require("express");
 const app = express();
+const bodyParser = require('body-parser')
 const port = process.env.PORT || 3001;
 
+app.use(bodyParser.json()) // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
+
 app.get("/", (req, res) => res.type('html').send(html));
+
+app.post('/mybody', (req, res)=>{
+  console.log(req.body)
+  res.status(200).json(req.body)
+});
 
 const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
